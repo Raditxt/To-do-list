5c43f9c6d22a415eb384cb12e76fec870fab6d54
Raditya Mulya Akbar <radityamulyaakbar2@gmail.com>
2024-06-25 23:35:14 +0800

    styling

--- script.js
+++ script.js
@@ -8,13 +8,20 @@ const togglescreen = () => {
 menubtn.addEventListener("click", togglescreen);
 
 // Toggle btn
-document.querySelectorAll('.category .toggle-btn').forEach(function(btn) {
-    btn.addEventListener('click', function() {
-      this.classList.toggle('active');
-      const icon = this.querySelector('i');
-      icon.classList.toggle('bx-toggle-left');
-      icon.classList.toggle('bx-toggle-right');
-    });
+document.querySelectorAll('.toggle-btn').forEach(function(btn) {
+  btn.addEventListener('click', function() {
+    this.classList.toggle('active');
+    const icon = this.querySelector('i');
+    if (this.classList.contains('active')) {
+      icon.classList.remove('bx-check-square');
+      icon.classList.add('bx-checkbox-checked');
+    } else {
+      icon.classList.remove('bx-checkbox-checked');
+      icon.classList.add('bx-check-square');
+    }
   });
+});
+
+
   
   
